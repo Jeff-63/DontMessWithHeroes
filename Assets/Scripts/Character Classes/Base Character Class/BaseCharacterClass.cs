da81fc58285a0f797d82a4b7b49d83db9bd05357
Samuel Perreault <samuelperreault@me.com>
2018-08-29 11:56:26 -0400

    BaseCharacterClass
    Story: SetUp
    Basics stats that will be shared between all characters.
    Rev:

---
+++ Assets/Scripts/Character Classes/Base Character Class/BaseCharacterClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterClass {
+
+    public string characterClass; // for player : spellcaster or warrior -- for ennemies : enemy type 
+    public string characterClassDescription;
+
+    //stats
+    public int characterLevel;
+    public int experience;
+    public int maxExperience; // will increase for each level up (1.5x current xp?)
+    public int strenght; // physical attack power
+    public int endurance; // physical defense 
+    public int intellienge; //magic attack power and magic defense
+    public int agility; // affects critical hits, accuracy of attacks and possibility of evading an attack
+    public int currentHP; // value of health at the current moment
+    public int maxHP; // max value for health, can increase with the levels
+
+}
