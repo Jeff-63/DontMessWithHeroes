2b2a0253525538c4898ad9c5dbf5cebb2b32d275
Samuel Perreault <samuelperreault@me.com>
2018-08-30 11:59:17 -0400

    PlayerMovement
    Player : 3
    
    Implement basics of movement for player.

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager {
+    Player player;
+    Vector3 startingPos = new Vector3();
+
+    public void Init()
+    {
+        CreatePlayer();
+    }
+    public void Update()
+    {
+
+    }
+    public void CreatePlayer()
+    {
+        GameObject playerObj = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/Player")); //crée instance joueur
+        player.transform.position = startingPos;
+        player = playerObj.GetComponent<Player>();
+        player.Init();
+    }
+
+    
+}
