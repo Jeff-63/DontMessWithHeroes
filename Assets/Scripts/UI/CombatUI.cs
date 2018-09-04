05adec1e9a8e0aa62bca4a7bf0968a45eb198935
Jeff-63 <40066325+Jeff-63@users.noreply.github.com>
2018-09-04 13:23:17 -0400

    Refactor
    Rev:jf
    UI:24
    refactor combat flow using UIManager

--- Assets/Scripts/UI/CombatUI.cs
+++ Assets/Scripts/UI/CombatUI.cs
@@ -17,7 +17,7 @@ public class CombatUI : MonoBehaviour
 
     Vector2 playerStartingPosition,  ennemiStartingPosition;
 
-    void Start()
+    /*void Start()
     {
         UIWidth = CombatFlow.gl.CombatUiCanvas.pixelRect.size.x;
         pixelPerUnitUI = CombatFlow.gl.CombatUiCanvas.referencePixelsPerUnit;
@@ -85,14 +85,78 @@ public class CombatUI : MonoBehaviour
         ennemiObj.transform.position = ennemiStartingPosition;
         ennemiObj.transform.localScale *= characterSize;
 
-    }
-
-    /*public void Initialize()
-    {      
-
     }*/
 
-    private void Update()
+    public void Initialize()
+    {      
+        UIWidth = CombatFlow.gl.CombatUiCanvas.pixelRect.size.x;
+        pixelPerUnitUI = CombatFlow.gl.CombatUiCanvas.referencePixelsPerUnit;
+
+        player = new Warrior();
+        ennemi = new Orc();
+
+        player.currentHP -= 10;
+        ennemi.currentHP -= 15;
+
+        playerStartingPosition = new Vector2((-UIWidth/ pixelPerUnitUI), 0);
+        ennemiStartingPosition = new Vector2((UIWidth / pixelPerUnitUI), 0);
+
+        playerHp = CombatFlow.gl.playerHp.GetComponent<Text>(); //pointeur vers le composant text des HP du joueur
+        playerMana = CombatFlow.gl.playerMana.GetComponent<Text>(); //pointeur vers le composant text de la mana du joueur
+        ennemiHP = CombatFlow.gl.ennemiHP.GetComponent<Text>(); //pointeur vers le composant text des HP de l'ennemi
+        ennemiMana = CombatFlow.gl.ennemiMana.GetComponent<Text>(); //pointeur vers le composant text de la mana de l'ennemi
+
+        playerHpImage = CombatFlow.gl.playerHpImage.GetComponent<Image>(); //pointeur vers le composant image des HP du joueur
+        playerManaImage = CombatFlow.gl.playerManaImage.GetComponent<Image>(); //pointeur vers le composant image de la mana du joueur
+        ennemiHPImage = CombatFlow.gl.ennemiHPImage.GetComponent<Image>(); //pointeur vers le composant image des HP de l'ennemi
+        ennemiManaImage = CombatFlow.gl.ennemiManaImage.GetComponent<Image>(); //pointeur vers le composant image de la mana de l'ennemi
+        playerImage = CombatFlow.gl.playerImage.GetComponent<Image>(); //pointeur vers le composant image du portrait du joueur
+        ennemiImage = CombatFlow.gl.ennemiImage.GetComponent<Image>(); //pointeur vers le composant image du portrait de l'ennemi
+
+        switch (player.characterClass)
+        {
+            case CharacterClasses.Warrior:
+                playerImage.sprite = Resources.Load<Sprite>("Sprites/Gladiator_Portrait");
+                break;
+
+            case CharacterClasses.Wizard:
+                playerImage.sprite = Resources.Load<Sprite>("Sprites/Wizards_Portrait");
+                break;
+
+            default:
+                Debug.Log("Unhandeled character class : " + player.characterClass);
+                break;
+        }
+
+        switch (ennemi.characterClass)
+        {
+            case CharacterClasses.Orc:
+                ennemiImage.sprite = Resources.Load<Sprite>("Sprites/Orc_Portrait");
+                break;
+
+            case CharacterClasses.Elemental:
+                ennemiImage.sprite = Resources.Load<Sprite>("Sprites/Elemental_Portrait");
+                break;
+
+            case CharacterClasses.Boss:
+                ennemiImage.sprite = Resources.Load<Sprite>("Sprites/Golem_Portrait");
+                break;
+
+            default:
+                Debug.Log("Unhandeled character class : " + ennemi.characterClass);
+                break;
+        }
+
+        GameObject playerObj = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/Warrior")); 
+        playerObj.transform.position = playerStartingPosition;
+        playerObj.transform.localScale *= characterSize;
+
+        GameObject ennemiObj = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/Orc"));
+        ennemiObj.transform.position = ennemiStartingPosition;
+        ennemiObj.transform.localScale *= characterSize;
+    }
+
+    /*private void Update()
     {
         playerHp.text = OmniPlayer.Instance.currentHP.ToString() + " / " + OmniPlayer.Instance.maxHP.ToString(); //update du texte des pv du personnage
         ennemiHP.text = OmniEnemy.Instance.currentHP.ToString() + " / " + OmniEnemy.Instance.maxHP.ToString(); //update du texte des pv de l'ennemi
@@ -106,16 +170,22 @@ public class CombatUI : MonoBehaviour
         playerManaImage.fillAmount = (OmniPlayer.Instance.currentMana / OmniPlayer.Instance.maxMana);//update mana bar player  
         ennemiManaImage.fillAmount = (OmniEnemy.Instance.currentMana / OmniEnemy.Instance.maxMana);//update mana bar enemy
             
-    }
-
-    /*public void UpdateUI(float dt)
-    {
-        playerHp.text = player.currentHP.ToString() + " / " + player.maxHP.ToString(); //update du texte des pv du personnage
-        ennemiHP.text = ennemi.currentHP.ToString() + " / " + ennemi.maxHP.ToString(); //update du texte des pv de l'ennemi
-
-        playerHpImage.fillAmount = ((float)player.currentHP / (float)player.maxHP); //update de l'image des pv du personnage
-        ennemiHPImage.fillAmount = ((float)ennemi.currentHP / (float)ennemi.maxHP); //update de l'image des pv de l'ennemi
     }*/
+
+    public void UpdateUI(float dt)
+    {
+         playerHp.text = OmniPlayer.Instance.currentHP.ToString() + " / " + OmniPlayer.Instance.maxHP.ToString(); //update du texte des pv du personnage
+        ennemiHP.text = OmniEnemy.Instance.currentHP.ToString() + " / " + OmniEnemy.Instance.maxHP.ToString(); //update du texte des pv de l'ennemi
+
+        playerMana.text = OmniPlayer.Instance.currentMana.ToString() + " / " + OmniPlayer.Instance.maxMana.ToString();//update txt mana player
+        ennemiMana.text = OmniEnemy.Instance.currentMana.ToString() + " / " + OmniEnemy.Instance.maxMana.ToString();//update txt mana enemy
+
+        playerHpImage.fillAmount = ((float)OmniPlayer.Instance.currentHP / (float)OmniPlayer.Instance.maxHP); //update de l'image des pv du personnage
+        ennemiHPImage.fillAmount = ((float)OmniEnemy.Instance.currentHP / (float)OmniEnemy.Instance.maxHP); //update de l'image des pv de l'ennemi
+
+        playerManaImage.fillAmount = (OmniPlayer.Instance.currentMana / OmniPlayer.Instance.maxMana);//update mana bar player  
+        ennemiManaImage.fillAmount = (OmniEnemy.Instance.currentMana / OmniEnemy.Instance.maxMana);//update mana bar enemy
+    }
 
     /*public void FixedUpdateUI(float dt)
     {
